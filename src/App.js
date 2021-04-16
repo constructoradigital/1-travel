5fc6020ead59f0a6c2fbaefdea7078868466b433
clicaps <77038650+Yeltzin1@users.noreply.github.com>
2021-04-16 11:30:49 -0600

    RouterHash agregado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import {Switch,Route, HashRouter} from 'react-router-dom'
 import Home from './components/pages/Home'
 import { Services } from './components/pages/Services';
 import { Products } from './components/pages/Products';
@@ -10,7 +10,7 @@ import { SignUp } from './components/pages/SignUp';
 function App() {
   return (
     <>
-    <Router>
+    <HashRouter>
        <Navbar/>
         <Switch>
           <Route path="/" exact component={Home} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="/products"  component={Products} />
           <Route path="/sign-up"  component={SignUp} />
         </Switch>
-    </Router>
+    </HashRouter>
     </>
   );
 }
